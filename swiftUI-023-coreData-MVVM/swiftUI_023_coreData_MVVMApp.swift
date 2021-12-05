b2137f90e41839b5f556def1df74adfe7c480a3e
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2021-12-05 14:53:37 -0300

    Initial Commit

---
+++ swiftUI-023-coreData-MVVM/swiftUI_023_coreData_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_023_coreData_MVVMApp.swift
+//  swiftUI-023-coreData-MVVM
+//
+//  Created by Luiz Araujo on 05/12/21.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_023_coreData_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
